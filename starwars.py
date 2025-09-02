8e6dde39794e3bfe516fca28d73c39ced0170541
Leandro Ribeiro Lima Junior <leandro.junior@peers.com.br>
2025-09-02 10:36:36 -0300

    Feat: arvore star wars pt2

--- starwars.py
+++ starwars.py
@@ -10,8 +10,7 @@ print(df.columns)
 
 # %%
 features = ['Massa(em kilos)', 'Estatura(cm)', 'Tempo de existência(em meses)', 
-            'Distância Ombro a ombro', 'Tamanho dos pés', 'Tamanho do crânio', 
-            'General Jedi encarregado']
+            'Distância Ombro a ombro', 'Tamanho dos pés', 'Tamanho do crânio', ]
 target = 'Status '
 
 x = df[features]
@@ -22,21 +21,23 @@ x = x.replace({
     "Tipo 1":1 , "Tipo 2":2, "Tipo 3":3, "Tipo 4":4, "Tipo 5":5,
     "Tipo 1":1 , "Tipo 2":2, "Tipo 3":3, "Tipo 4":4, "Tipo 5":5,
     "Tipo 1":1 , "Tipo 2":2, "Tipo 3":3, "Tipo 4":4, "Tipo 5":5,
-    "Yoda":1, "Shaak Ti":2, "Obi-Wan Kenobi":3, "Aayla Secura":4, "Mace Windu":5,
 })
 
 
 # %%
-model = tree.DecisionTreeClassifier()
+model = tree.DecisionTreeClassifier(
+    max_depth= 5,
+    random_state=42
+)
 model.fit(X=x, y=y)
 
 # %%
 import matplotlib.pyplot as plt
 
-plt.figure(dpi=400)
+plt.figure(dpi=1000)
 
 tree.plot_tree(model, feature_names=features,
                class_names=model.classes_,
-               filled=True,
+               filled=True
                )
 # %%
